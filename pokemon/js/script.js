912e8d6ec346e5cfd5b015adabc859d1dcc1878c
Isalvan <isalvan12345@gmail.com>
2025-01-14 14:56:21 +0100

    he subido la pokedex acabada

--- pokemon/js/script.js
+++ pokemon/js/script.js
@@ -1,74 +1,222 @@
-const listaPokemon = document.querySelector('#listaPokemon');
-const botonesHeader = document.querySelectorAll('.btn-header');
-let URL = 'https://pokeapi.co/api/v2/pokemon/';
-let contador=0;
+const listaPokemon = document.querySelector('#listaPokemon'); // Seleccionamos el div donde se van a mostrar los pokemons
+const botonesTipo = document.querySelectorAll('.btn-tipo'); // Seleccionamos los botones del header
+const botonesGen = document.querySelectorAll('.btn-gen'); // Seleccionamos los botones de generación
+const buscador = document.querySelector('#barraBusqueda');
+const loadingIndicator = document.querySelector('#loading');
+const worker = new Worker('js/worker.js');
+let pokemons = []; // Array para almacenar los datos de los pokemons
+let currentPage = 1;
+const pokemonsPerPage = 30;
 
-for (let i=1; i<=155; i++){
-    fetch(URL + i)
-        .then(response => response.json())
-        .then(poke => mostrarPokemon(poke))
-        contador++;
+buscador.addEventListener('input', (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    filtrarPokemon(searchTerm);
+});
 
-}
-console.log(contador);
+document.addEventListener('DOMContentLoaded', () => {
+    if (!localStorage.getItem('allPokemons')) {
+        worker.postMessage({ tipo: 'cargarTodos' });
+    } else {
+        renderPage(1);
+    }
+});
 
-function mostrarPokemon(poke){
+worker.onmessage = function (event) {
+    const pokemones = event.data;
+    localStorage.setItem('allPokemons', JSON.stringify(pokemones));
+    pokemons = pokemones;
+    renderPage(currentPage);
+    loadingIndicator.style.display = 'none';
+};
 
-    let tipos = poke.types.map(type => `
-            <p class="${type.type.name} tipo">${type.type.name}</p>
-        `).join('');
-
-    let pokeId = poke.id.toString();
-
-    if(pokeId.length === 1){
-        pokeId = '00' + pokeId;
-    }else if(pokeId.length === 2){
-        pokeId = '0' + pokeId;
+function iniciar() {
+    const cachedPokemones = localStorage.getItem('allPokemons');
+    if (cachedPokemones) {
+        const pokemones = JSON.parse(cachedPokemones);
+        pokemons = pokemones;
+        renderPage(currentPage);
+        loadingIndicator.style.display = 'none';
+    } else {
+        worker.postMessage('loadPokemons');
+        loadingIndicator.style.display = 'block';
     }
 
-    const div = document.createElement("div");
-    div.classList.add("pokemon");
-    div.innerHTML=`
-        <p class="pokemon-id-back">#${pokeId}</p>
-        <div class="pokemon-imagen">
-            <img src="${poke.sprites.other["official-artwork"].front_default}" alt="${poke.name}">
-        </div>
-        <div class="pokemon-info">
-            <div class="nombre-contenedor">
-                <p class="pokemon-id">#${pokeId}</p>
-                <h2 class="pokemon-nombre">${poke.name}</h2>
-            </div>
-            <div class="pokemon-tipos">
-                ${tipos}
-            </div>
-            <div class="pokemon-stats">
-                <p class="stat">${poke.height}</p>
-                <p class="stat">${poke.weight}</p>
-            </div>
-        </div>
-    `;
+    botonesTipo.forEach(boton => {
+        boton.addEventListener('click', (event) => {
+            filtrarPorTipo(event.target.id)
+        });
+    });
 
-    listaPokemon.append(div);
+    botonesGen.forEach(boton => {
+        boton.addEventListener('click', (event) => {
+            filtrarPorGeneracion(event.target.id);
+        });
+    });
 }
 
-botonesHeader.forEach(boton => boton.addEventListener('click', (event) => {
-    const botonId = event.currentTarget.id;
+function mostrarPokemon(pokemons) {
+    listaPokemon.innerHTML = ''; // Clear previous content
+    pokemons.forEach(poke => {
+        let tipos = poke.types.map(type => `<p class="${type} tipo">${type}</p>`).join('');
 
+        let pokeId = poke.id.toString().padStart(3, '0'); // Asegura que el ID tenga 3 dígitos
+
+        const div = document.createElement("div");
+        div.classList.add("pokemon");
+
+        div.innerHTML = `
+            <p class="pokemon-id-back">#${pokeId}</p>
+            <div class="pokemon-imagen">
+                <img src="${poke.image}" alt="${poke.name}">
+            </div>
+            <div class="pokemon-info">
+                <div class="nombre-contenedor">
+                    <p class="pokemon-nombre">${poke.name}</p>
+                </div>
+                <div class="pokemon-tipos">
+                    ${tipos}
+                </div>
+            </div>
+        `;
+
+        div.addEventListener('click', () => {
+            window.location.href = `./detalle.html?id=${poke.id}`;
+        });
+
+        listaPokemon.appendChild(div);
+    });
+}
+
+function renderPage(page) {
+    const start = (page - 1) * pokemonsPerPage;
+    const end = start + pokemonsPerPage;
+    const paginatedPokemons = pokemons.slice(start, end);
+    mostrarPokemon(paginatedPokemons);
+    renderPagination();
+}
+
+function renderPagination() {
+    const totalPages = Math.ceil(pokemons.length / pokemonsPerPage);
+    const paginationContainer = document.querySelector('#pagination');
+    paginationContainer.innerHTML = '';
+
+    const prevButton = document.createElement('button');
+    prevButton.textContent = '❮';
+    prevButton.classList.add('pagination-arrow');
+    prevButton.disabled = currentPage === 1;
+    prevButton.addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage--;
+            renderPage(currentPage);
+        }
+    });
+    paginationContainer.appendChild(prevButton);
+
+    const nextButton = document.createElement('button');
+    nextButton.textContent = '❯';
+    nextButton.classList.add('pagination-arrow');
+    nextButton.disabled = currentPage === totalPages;
+    nextButton.addEventListener('click', () => {
+        if (currentPage < totalPages) {
+            currentPage++;
+            renderPage(currentPage);
+        }
+    });
+    paginationContainer.appendChild(nextButton);
+}
+
+function filtrarPokemon(searchTerm) {
+    const pokemones = JSON.parse(localStorage.getItem('allPokemons'));
+    listaPokemon.innerHTML = '';
+    
+    if (searchTerm === '') {
+        renderPage(1);
+        return;
+    }
+
+    const filteredPokemones = isNaN(searchTerm) 
+        ? pokemones.filter(poke => poke.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        : pokemones.filter(poke => poke.id === parseInt(searchTerm));
+    
+    currentPage = 1; // Reset to the first page when filtering
+    renderFilteredPage(filteredPokemones, currentPage);
+}
+
+function filtrarPorGeneracion(generacionId) {
+    const pokemones = JSON.parse(localStorage.getItem('allPokemons'));
     listaPokemon.innerHTML = '';
 
-    for (let i=1; i<=30; i++){
-        fetch(URL + i)
-            .then((response) => response.json())
-            .then(poke => {
+    const GENERACIONES = {
+        gen1: [1, 151],
+        gen2: [152, 251],
+        gen3: [252, 386],
+        gen4: [387, 493],
+        gen5: [494, 649],
+        gen6: [650, 721],
+        gen7: [722, 809],
+        gen8: [810, 898],
+        gen9: [899, 1025]
+    };
 
-                if(botonId === 'ver-todos'){
-                    mostrarPokemon(poke);
-                }else{
-                    const tipos = poke.types.map(type => type.type.name)
-                    if(tipos.some(tipo => tipo.includes(botonId))){
-                        mostrarPokemon(poke);
-                    }
-                }
-            })
+    const [inicio, fin] = GENERACIONES[generacionId];
+    const filteredPokemones = pokemones.filter(poke => poke.id >= inicio && poke.id <= fin);
+
+    currentPage = 1; // Reset to the first page when filtering
+    renderFilteredPage(filteredPokemones, currentPage);
+}
+
+function filtrarPorTipo(tipo) {
+    const pokemones = JSON.parse(localStorage.getItem('allPokemons'));
+    listaPokemon.innerHTML = '';
+    
+    if (tipo === 'ver-todos') {
+        renderPage(1);
+        return;
     }
-}))
+    console.log(pokemones);
+    const filteredPokemones = pokemones.filter(poke => poke.types.includes(tipo));
+    
+    currentPage = 1; // Reset to the first page when filtering
+    renderFilteredPage(filteredPokemones, currentPage);
+}
+
+function renderFilteredPage(filteredPokemones, page) {
+    const start = (page - 1) * pokemonsPerPage;
+    const end = start + pokemonsPerPage;
+    const paginatedPokemons = filteredPokemones.slice(start, end);
+    mostrarPokemon(paginatedPokemons);
+    renderFilteredPagination(filteredPokemones);
+}
+
+function renderFilteredPagination(filteredPokemones) {
+    const totalPages = Math.ceil(filteredPokemones.length / pokemonsPerPage);
+    const paginationContainer = document.querySelector('#pagination');
+    paginationContainer.innerHTML = '';
+
+    const prevButton = document.createElement('button');
+    prevButton.textContent = '←';
+    prevButton.classList.add('pagination-arrow');
+    prevButton.disabled = currentPage === 1;
+    prevButton.addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage--;
+            renderFilteredPage(filteredPokemones, currentPage);
+        }
+    });
+    paginationContainer.appendChild(prevButton);
+
+    const nextButton = document.createElement('button');
+    nextButton.textContent = '→';
+    nextButton.classList.add('pagination-arrow');
+    nextButton.disabled = currentPage === totalPages;
+    nextButton.addEventListener('click', () => {
+        if (currentPage < totalPages) {
+            currentPage++;
+            renderFilteredPage(filteredPokemones, currentPage);
+        }
+    });
+    paginationContainer.appendChild(nextButton);
+}
+
+// Call iniciar to start the process
+iniciar();
