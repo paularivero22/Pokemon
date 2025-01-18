8e4c3c5654ccff2bcfe3ab0127f329332a8a7891
franrollo99 <47180040+franrollo99@users.noreply.github.com>
2025-01-18 18:51:48 +0100

    Comparador acabado y detalles y worker actualizado
    Pagina detalles cambiada y hecha con el worker

--- pokemon/js/comparador.js
+++ pokemon/js/comparador.js
@@ -1,8 +1,9 @@
-// https://veekun.com/dex/gadgets/compare_pokemon?pokemon=bulbasur&pokemon=charmander
 const buscadores = document.querySelectorAll('.barraBusqueda');
 const listasPokemon = document.querySelectorAll('.pokemon-todos');
 const divsComparadores = document.querySelectorAll('.comparadorPokemon');
 const loading = document.querySelectorAll('.loading');
+let pokemonSeleccionado1 = null;
+let pokemonSeleccionado2 = null;
 
 const worker = new Worker('js/worker.js');
 let pokemons = []; // Array para almacenar los datos de los pokemons
@@ -23,8 +24,23 @@ buscadores.forEach((buscador, indice) => {
         const searchTerm = event.target.value.toLowerCase();
         const listaPokemon = listasPokemon[indice];
         const divComparador = divsComparadores[indice];
-        filtrarPokemon(searchTerm, buscador, listaPokemon, divComparador);
+        filtrarPokemon(searchTerm, buscador, listaPokemon, divComparador, indice);
     });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (!localStorage.getItem('allPokemons')) {
+        worker.postMessage({ tipo: 'cargarTodos' });
+    }
+
+    // Cargar Pokémon para comparar desde detalles
+    const pokemonComparar = localStorage.getItem('pokemonParaComparar');
+    if (pokemonComparar) {
+        const { poke, indice } = JSON.parse(pokemonComparar);
+        mostrarPokemonAComparar(poke, divsComparadores[indice], indice);
+        // Limpiar localStorage para evitar conflictos futuros
+        localStorage.removeItem('pokemonParaComparar');
+    }
 });
 
 worker.onmessage = function (event) {
@@ -34,8 +50,9 @@ worker.onmessage = function (event) {
 
     loading.forEach(indicador => {
         indicador.style.display = 'none';
-    })
+    });
 };
+
 
 function iniciar() {
     const cachedPokemones = localStorage.getItem('allPokemons');
@@ -53,7 +70,7 @@ function iniciar() {
     }
 }
 
-function filtrarPokemon(searchTerm, buscador, listaPokemon, divComparador) {
+function filtrarPokemon(searchTerm, buscador, listaPokemon, divComparador, indice) {
     const pokemones = JSON.parse(localStorage.getItem('allPokemons'));
     listaPokemon.innerHTML = '';
     
@@ -62,17 +79,17 @@ function filtrarPokemon(searchTerm, buscador, listaPokemon, divComparador) {
         : pokemones.filter(poke => poke.id === parseInt(searchTerm));
     
     currentPage = 1; // Reset to the first page when filtering
-    renderFilteredPage(filteredPokemones, currentPage, buscador, listaPokemon, divComparador);
+    renderFilteredPage(filteredPokemones, currentPage, buscador, listaPokemon, divComparador, indice);
 }
 
-function renderFilteredPage(filteredPokemones, page, buscador, listaPokemon, divComparador) {
+function renderFilteredPage(filteredPokemones, page, buscador, listaPokemon, divComparador, indice) {
     const start = (page - 1) * pokemonsPerPage;
     const end = start + pokemonsPerPage;
     const paginatedPokemons = filteredPokemones.slice(start, end);
-    mostrarPokemon(paginatedPokemons, buscador, listaPokemon, divComparador);
+    mostrarPokemon(paginatedPokemons, buscador, listaPokemon, divComparador, indice);
 }
 
-function mostrarPokemon(pokemons, buscador, listaPokemon, divComparador) {
+function mostrarPokemon(pokemons, buscador, listaPokemon, divComparador, indice) {
     listaPokemon.innerHTML = ''; // Clear previous content
     pokemons.forEach(poke => {
         let tipos = poke.types.map(type => `<p class="${type} tipo">${type}</p>`).join('');
@@ -95,7 +112,7 @@ function mostrarPokemon(pokemons, buscador, listaPokemon, divComparador) {
         `;
 
         div.addEventListener('click', () => {
-            mostrarPokemonAComparar(poke, divComparador)
+            mostrarPokemonAComparar(poke, divComparador, indice)
             listaPokemon.innerHTML='';
             buscador.value='';
         });
@@ -104,15 +121,17 @@ function mostrarPokemon(pokemons, buscador, listaPokemon, divComparador) {
     });
 }
 
-function mostrarPokemonAComparar(poke, divComparador) {
+function mostrarPokemonAComparar(poke, divComparador, indice) {
     let tipos = poke.types.map(type => `<p class="${type} tipo">${type}</p>`).join('');
     let stats = poke.stats.map(stat => {
         const statsEsp = statsTraducidas[stat.stat.name] || stat.stat.name;
-        return `<p class="stat"><b>${statsEsp}:</b> ${stat.base_stat}</p>`;
+        return `<p class="stat" data-stat="${stat.stat.name}">${statsEsp}: ${stat.base_stat}</p>`;
     }).join('');
     let totalStats = 0;
-    // poke.stats.forEach(stat => stat.base_stat);
-    
+    poke.stats.forEach(stat => {
+        totalStats += stat.base_stat;
+    });
+
     divComparador.innerHTML = `
         <div class="pokemon-seleccionado">
             <div class="pokemon-imagen">
@@ -125,12 +144,71 @@ function mostrarPokemonAComparar(poke, divComparador) {
                 </div>
                 <div class="pokemon-stats">
                     ${stats}
-                    <p class="totalStats"><b>Base Total Estadisticas:</b> ${totalStats}</p>
+                    <p class="stat">Base Total Estadisticas: ${totalStats}</p>
                 </div>
             </div>
         </div>
     `;
+
+    if(indice === 0){
+        pokemonSeleccionado1 = poke;
+    }else{
+        pokemonSeleccionado2 = poke;
+    }
+
+    if(pokemonSeleccionado1 && pokemonSeleccionado2){
+        compararPokemons();
+    }
 }
+
+function compararPokemons() {
+
+    const stats1 = pokemonSeleccionado1.stats;
+    const stats2 = pokemonSeleccionado2.stats;
+
+    const div1 = divsComparadores[0].querySelector('.pokemon-stats');
+    const div2 = divsComparadores[1].querySelector('.pokemon-stats');
+
+    // Comparar cada estadística
+    stats1.forEach((stat1, index) => {
+        const stat2 = stats2[index];
+
+        const statElement1 = div1.querySelector(`[data-stat="${stat1.stat.name}"]`);
+        const statElement2 = div2.querySelector(`[data-stat="${stat2.stat.name}"]`);
+
+        if (stat1.base_stat > stat2.base_stat) {
+            statElement1.style.color = 'green';
+            statElement2.style.color = 'red';
+        } else if (stat1.base_stat < stat2.base_stat) {
+            statElement1.style.color = 'red';
+            statElement2.style.color = 'green';
+        } else {
+            statElement1.style.color = 'gray';
+            statElement2.style.color = 'gray';
+        }
+    });
+
+    // Comparar la suma total de estadísticas
+    const total1 = stats1.reduce((total, stat) => total + stat.base_stat, 0);
+    const total2 = stats2.reduce((total, stat) => total + stat.base_stat, 0);
+
+    const comparador1 = divsComparadores[0].querySelector('.pokemon-seleccionado');
+    const comparador2 = divsComparadores[1].querySelector('.pokemon-seleccionado');
+
+    if (total1 > total2) {
+        comparador1.style.backgroundColor = 'lightgreen';
+        comparador2.style.backgroundColor = 'lightcoral';
+    } else if (total1 < total2) {
+        comparador1.style.backgroundColor = 'lightcoral';
+        comparador2.style.backgroundColor = 'lightgreen';
+    } else {
+        comparador1.style.backgroundColor = 'gray';
+        comparador2.style.backgroundColor = 'gray';
+        }
+}
+
+
+
 
 // Call iniciar to start the process
 iniciar();
